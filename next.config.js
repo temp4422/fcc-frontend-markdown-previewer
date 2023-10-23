47560c601189182e7481745bea17860d2470986c
user <user@MacBook-Pro-user.local>
2023-10-23 15:58:17 +0300

    Reset configs for GitHub Action Workflow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
-  basePath: '/fcc-frontend-markdown-previewer',
 }
 
 module.exports = nextConfig
